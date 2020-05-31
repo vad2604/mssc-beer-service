9b2f9962ebe74e27bbca3a32b43118d6ee43a814
vad2604 <vad2604@yandex.ru>
2020-05-31 22:08:24 +0400

    adding web model

---
+++ src/main/java/com/example/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,31 @@
+package com.example.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private String version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+
+    private Long upc;
+    private BigDecimal price;
+
+    private Integer quanityOnHand;
+}
